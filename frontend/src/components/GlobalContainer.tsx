1d7c6dbc245d9bb2e7ccb6ec753cd3512c15f284
sugichan-a <sugihara200303@gmail.com>
2025-07-19 17:16:51 +0900

    タイトルの動的設定を追加し、グローバルヘッダーに反映

--- frontend/src/components/GlobalContainer.tsx
+++ frontend/src/components/GlobalContainer.tsx
@@ -1,15 +1,26 @@
+"use client";
+// GlobalContainer.tsx
 import { Container } from "@mui/material";
 import { VerticalSpacer } from "../components/VerticalSpacer";
 import { GlobalHeader } from "../components/GlobalHeader";
 import { GlobalFooter } from "../components/GlobalFooter";
+import { useEffect, useState } from "react";
 
 export function GlobalContainer({ children }: { children?: React.ReactNode }) {
+  const [title, setTitle] = useState("Talenza");
+
+  useEffect(() => {
+    if (typeof document !== "undefined") {
+      setTitle(document.title);
+    }
+  }, []);
+
   return (
     <Container
       sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
     >
       <header>
-        <GlobalHeader title={"Talenza"} />
+        <GlobalHeader title={title} />
       </header>
 
       <VerticalSpacer height={32} />
